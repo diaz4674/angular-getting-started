8804647d7e45979aa54a8a095e20ef6760bcea80
Miguel Diaz <diaz4674@gmail.com>
2021-06-30 21:21:04 +0000

    images going through

--- apm-new/src/app/products/product-list.component.ts
+++ apm-new/src/app/products/product-list.component.ts
@@ -7,20 +7,24 @@ import { Component } from '@angular/core';
 
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
   products: any[] = [{
     "productID": 1,
     "productName": "Garden Cart",
     "productCode": "GDN-9923",
+    "imageURL": "assets/images/garden_cart.png",
     "price": 32.99,
     "starRating": 4.2
   },
 
-{
+  {
     "productID": 1,
     "productName": "Garden Cart",
     "productCode": "GDN-9923",
+    "imageURL": "assets/images/garden_cart.png",
     "price": 32.99,
     "starRating": 4.2
   }
-];
+  ];
 }
