678a0b77e5a8d21f3fb9f1234aaf6be0720fab40
miguel diaz <diaz4674@gmail.com>
2021-06-30 22:16:02 -0700

    filtering functional

--- apm-new/src/app/products/product-list.component.ts
+++ apm-new/src/app/products/product-list.component.ts
@@ -1,35 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './products';
 
 @Component({
   selector: "pm-products",
-  templateUrl: "./product-list.component.html"
+  templateUrl: "./product-list.component.html",
+  styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
-  products: any[] = [{
-    "productID": 1,
+  
+  private _listFilter: string = ''; //
+
+  get listFilter(): string { return this._listFilter; }
+
+  set listFilter(value: string){
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] =[
+  {
+    "productId": 1,
     "productName": "Garden Cart",
     "productCode": "GDN-9923",
-    "imageURL": "assets/images/garden_cart.png",
+    "releaseDate": "March 18, 2021",
+    "description": "15 gallon cap rolling garden cart",
+    "imageUrl": "assets/images/garden_cart.png",
     "price": 32.99,
     "starRating": 4.2
   },
-
   {
-    "productID": 1,
+    "productId": 1,
     "productName": "Garden Cart",
     "productCode": "GDN-9923",
-    "imageURL": "assets/images/garden_cart.png",
+    "releaseDate": "March 18, 2021",
+    "description": "15 gallon cap rolling garden cart",
+    "imageUrl": "assets/images/garden_cart.png",
     "price": 32.99,
     "starRating": 4.2
   }
   ];
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[]{
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: IProduct) => 
+      product.productName.toLowerCase().includes(filterBy))
+  }
+
+  ngOnInit(): void {
+    this.listFilter = 'cart'
   }
 }
