db27e59838a2b0b696f5cc6d0b75a8360990c519
miguel diaz <diaz4674@gmail.com>
2021-07-15 21:47:17 -0700

    routing works

--- apm-new/src/app/products/product-detail.component.ts
+++ apm-new/src/app/products/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './products';
+import {ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  constructor() { }
+  product: IProduct | undefined;
+  errorMessage='';
+
+  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`
+    // if(id){
+    //   this.getProduct(id);
+    // }
+  }
+
+  // getProduct(id: number): void {
+  //   this.productService.getProducts(id).subscribe({
+  //     next: product => this.product = product,
+  //     error: err => this.errorMessage = err
+  //   })
+  // }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
